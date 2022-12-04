0b7a9567f6b471d8a5592015e17f683eceb2a254
MirMoshref <117489824+MirMoshref@users.noreply.github.com>
2022-12-04 11:20:18 -0800

    Update ReviewClass.java
    Main method and review method removed

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,11 +2,5 @@ package review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
-		
-	}
 	
-	public void test() {
-		System.out.println("This method is on Review Session Branch");
-	}
 }
