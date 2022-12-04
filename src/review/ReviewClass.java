460698400e4790acb3e6b0109ad40c349a347136
MirMoshref <jalil.afgs@gmail.com>
2022-12-04 10:31:11 -0800

    in Review Session Branch

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -4,7 +4,9 @@ public class ReviewClass {
 
 	public static void main(String[] args) {
 		
-		
-		
+	}
+	
+	public void test() {
+		System.out.println("This method is on Review Session Branch");
 	}
 }
